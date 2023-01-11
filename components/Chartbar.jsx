b039b1e085563721e4d1e2e1cdfbb49daf769162
Yarline <yarlinecineus@outlook.fr>
2023-01-11 15:34:33 +0100

    chartbar test fake data

--- components/Chartbar.jsx
+++ components/Chartbar.jsx
@@ -5,54 +5,117 @@ export default function Chartbar() {
   const canvasEl = useRef(null);
 
   const colors = {
-    purple: {
-      default: "rgba(149, 76, 233, 1)",
-      half: "rgba(149, 76, 233, 0.5)",
-      quarter: "rgba(149, 76, 233, 0.25)",
-      zero: "rgba(149, 76, 233, 0)"
+    darkGreen: {
+      default: "#6AB04C",
     },
-    indigo: {
-      default: "rgba(80, 102, 120, 1)",
-      quarter: "rgba(80, 102, 120, 0.25)"
-    }
+    green: {
+      default: "#BADC58",
+    },
+    orange: {
+        default: "#F0932B",
+      },
+    red: {
+        default: "#FF7979",
+    },
+    darkRed: {
+        default: "#EB4D4B",
+    },
+    white: {
+        default: "#ffffff",
+    },
+    black: {
+        default: "#000000",
+    },
   };
 
   useEffect(() => {
     const ctx = canvasEl.current.getContext("2d");
     // const ctx = document.getElementById("myChart");
 
-    const gradient = ctx.createLinearGradient(0, 16, 0, 600);
-    gradient.addColorStop(0, colors.purple.half);
-    gradient.addColorStop(0.65, colors.purple.quarter);
-    gradient.addColorStop(1, colors.purple.zero);
-
-    const weight = [60.0, 60.2, 59.1, 61.4, 59.9, 60.2, 59.8, 58.6, 59.6, 59.2];
+    const weight = [60, 60.2, 59.1, 61.4, 59.9, 60.2, 59.8];
 
     const labels = [
-      "Week 1",
-      "Week 2",
-      "Week 3",
-      "Week 4",
-      "Week 5",
-      "Week 6",
-      "Week 7",
-      "Week 8",
-      "Week 9",
-      "Week 10"
+      "Nom de l'affirmation"
     ];
     const data = {
       labels: labels,
       datasets: [
         {
-          backgroundColor: gradient,
-          label: "My First Dataset",
+          backgroundColor: colors.darkGreen.default,
+          label: "D'accord",
           data: weight,
           fill: true,
           borderWidth: 2,
-          borderColor: colors.purple.default,
+          borderColor: colors.darkGreen.default,
           lineTension: 0.2,
-          pointBackgroundColor: colors.purple.default,
+          pointBackgroundColor: colors.darkGreen.default,
           pointRadius: 3
+        },
+        {
+            backgroundColor: colors.green.default,
+            label: "Plutôt d'accord",
+            data: weight,
+            fill: true,
+            borderWidth: 2,
+            borderColor: colors.green.default,
+            lineTension: 0.2,
+            pointBackgroundColor: colors.green.default,
+            pointRadius: 3
+        },
+        {
+            backgroundColor: colors.orange.default,
+            label: "Plutôt pas d'accord",
+            data: weight,
+            fill: true,
+            borderWidth: 2,
+            borderColor: colors.orange.default,
+            lineTension: 0.2,
+            pointBackgroundColor: colors.orange.default,
+            pointRadius: 3
+        },
+        {
+            backgroundColor: colors.red.default,
+            label: "Pas d'accord",
+            data: weight,
+            fill: true,
+            borderWidth: 2,
+            borderColor: colors.red.default,
+            lineTension: 0.2,
+            pointBackgroundColor: colors.red.default,
+            pointRadius: 3
+        },
+        {
+            backgroundColor: colors.darkRed.default,
+            label: "Totalement contre",
+            data: weight,
+            fill: true,
+            borderWidth: 2,
+            borderColor: colors.darkRed.default,
+            lineTension: 0.2,
+            pointBackgroundColor: colors.darkRed.default,
+            pointRadius: 3
+        },
+        {
+            backgroundColor: colors.white.default,
+            label: "Je ne sais pas",
+            data: weight,
+            fill: true,
+            borderWidth: 2,
+            borderColor: colors.black.default,
+            lineTension: 0.2,
+            pointBackgroundColor: colors.white.default,
+            pointRadius: 3
+        },
+        {
+            backgroundColor: colors.black.default,
+            label: "Je ne souhaite pas m'exprimer",
+            data: weight,
+            fill: true,
+            borderWidth: 2,
+            borderColor: colors.black.default,
+            lineTension: 0.2,
+            pointBackgroundColor: colors.black.default,
+            pointRadius: 3
         }
       ]
     };
--- pages/chart.jsx
+++ pages/chart.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 export default function chart() {
   return (
     <>
+    <h1>Titre du questionnaire</h1>
         <Chartbar/>
     </>
   )
