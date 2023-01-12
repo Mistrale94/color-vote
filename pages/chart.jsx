3775f1572b6df05ab9ab0cc86ad3704b1555d56b
Yarline <yarlinecineus@outlook.fr>
2023-01-12 11:22:13 +0100

    chart modif

--- pages/chart.jsx
+++ pages/chart.jsx
@@ -1,11 +1,15 @@
 import Chartbar from "../components/Chartbar"
+import Chartpie from "../components/Chartpie"
+import Tab from "../components/Tab"
 import React from 'react'
 
 export default function chart() {
   return (
     <>
     <h1>Titre du questionnaire</h1>
+        <Tab/>
         <Chartbar/>
+        <Chartpie/>
     </>
   )
 }
